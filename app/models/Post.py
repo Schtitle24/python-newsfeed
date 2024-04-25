9b23dd484573b1e796c6d0ec299443581fda17db
Schtitle24 <153530625+Schtitle24@users.noreply.github.com>
2024-04-25 10:21:56 -0500

    Vote Model added

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,15 +1,22 @@
 from datetime import datetime
 from app.db import Base
+from .Vote import Vote
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.sql import func
 
 class Post(Base):
-  __tablename__= 'posts'
-  id = Column(Integer, primary_key=True)
-  title = Column(String(100), nullable=False)
-  post_url = Column(String(100), nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-  user = relationship('User')
-  
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    post_url = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user = relationship('User')
+    comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
+    
+    @column_property
+    def vote_count(self):
+        return func.count(Vote.id).label('vote_count')
