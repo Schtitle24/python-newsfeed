527d0b3e23403ad1849b9fb1dcf6287bb34376cd
Schtitle24 <153530625+Schtitle24@users.noreply.github.com>
2024-04-26 11:25:08 -0500

    fixed points bug

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,22 +1,22 @@
 from datetime import datetime
 from app.db import Base
 from .Vote import Vote
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship, column_property
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
 
 class Post(Base):
-    __tablename__ = 'posts'
-    id = Column(Integer, primary_key=True)
-    title = Column(String(100), nullable=False)
-    post_url = Column(String(100), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    created_at = Column(DateTime, default=datetime.now)
-    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-    user = relationship('User')
-    comments = relationship('Comment', cascade='all,delete')
-    votes = relationship('Vote', cascade='all,delete')
-    
-    @column_property
-    def vote_count(self):
-        return func.count(Vote.id).label('vote_count')
+  __tablename__= 'posts'
+  id = Column(Integer, primary_key=True)
+  title = Column(String(100), nullable=False)
+  post_url = Column(String(100), nullable=False)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  created_at = Column(DateTime, default=datetime.now)
+  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+  
+  @property
+  def vote_count(self):
+      return len(self.votes)
+
+  user = relationship('User')
+  comments = relationship('Comment', cascade='all,delete')
+  votes = relationship('Vote', cascade='all,delete')
